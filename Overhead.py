169586a261da96365f278234d7e73efcb2dfcfa4
TeamC-Kevlyn <s10257388@connect.np.edu.sg>
2023-07-16 16:23:05 +0800

    tried to calculate percentage

--- Overhead.py
+++ Overhead.py
@@ -17,29 +17,30 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 
 def Overhead():
     """
-    Function calculate all overhead categories to find the highest overhead category
+    Function calculates all overhead categories and their percentages of the total amount
     """
-    Expenses = [] 
+
+    total_amount = sum(int(item[3]))
+
+    Expenses = []
     for item in Overhead_cost:
-        if item[1] not in Expenses: 
+        if item[1] not in Expenses:
             Expenses.append(item[1])
 
-    highest_total_amount = 0
-    highest_overhead_category = None
-
+    percentages = []
     for expense in Expenses:
-        total_amount = 0
-
+        category_amount = 0
         for item in Overhead_cost:
-            if expense == item[1]:
-                total_amount += int(item[3])
+            if item[1] == expense:
+                category_amount += int(item[3])
+        percentage = (category_amount / total_amount) * 100
+        percentages[expense] = round(percentage,2)
 
-        if total_amount > highest_total_amount:
-            highest_total_amount = total_amount
-            highest_overhead_category = expense
-    
-        Expenses.append((item[0], item[1], item[2], item[3]))
-    return Expenses
+        if percentage > highest_percentage:
+            highest_percentage = percentage
+            highest_category = item[0]
+    return highest_category
+print(Overhead())
 
 
 
