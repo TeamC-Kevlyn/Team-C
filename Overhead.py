feec5e6504c93d09b088f710c6af440eaefddefc
TeamC-Kevlyn <s10257388@connect.np.edu.sg>
2023-07-18 17:16:10 +0800

    complete version of overhead w/o command

--- Overhead.py
+++ Overhead.py
@@ -9,41 +9,33 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     for row in reader:
         Overhead_cost.append([row[0],row[1],row[2],row[3]])   
 
-# Category = [] 
-# for item in Overhead:
-#     if item[1] not in Category: 
-#         Category.append(item[1])
-# print(Category)
+def calculate_overhead_percentage(Overhead_cost):
+    total_amount = 0
+    expenses = []
+    highest_percentage = 0
+    highest_category = ""
 
-def Overhead():
-    """
-    Function calculates all overhead categories and their percentages of the total amount
-    """
-
-    total_amount = sum(int(item[3]))
-
-    Expenses = []
     for item in Overhead_cost:
-        if item[1] not in Expenses:
-            Expenses.append(item[1])
+        if item[1] not in expenses:
+            expenses.append(item[1])
 
-    percentages = []
-    for expense in Expenses:
+    for expense in expenses:
+        category_amount = 0
+        for item in Overhead_cost:
+            if item[1] == expense:
+                category_amount += int(item[3])
+        total_amount += category_amount
+
+    for expense in expenses:
         category_amount = 0
         for item in Overhead_cost:
             if item[1] == expense:
                 category_amount += int(item[3])
         percentage = (category_amount / total_amount) * 100
-        percentages[expense] = round(percentage,2)
 
         if percentage > highest_percentage:
-            highest_percentage = percentage
-            highest_category = item[0]
-    return highest_category
-print(Overhead())
+            highest_percentage = round(percentage,2)
+            highest_category = expense
 
-
-
-
-
-
+    return f'[HIGHEST OVERHEAD] {highest_category} : {highest_percentage}%' 
+print(calculate_overhead_percentage(Overhead_cost))
