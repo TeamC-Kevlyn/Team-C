3873877249fb067e25c901054d4ff3ca81f613c2
Syadnitsu <139613071+Syadnitsu@users.noreply.github.com>
2023-08-05 14:37:02 +0800

    Update mainSolution.py

--- mainSolution.py
+++ mainSolution.py
@@ -4,9 +4,14 @@ import profits_loss
 
 def main():
 
-    Overhead.Overhead()
-    CashOnHand.cashonhand()
-    profits_loss.profit_loss()
+    overhead_result = Overhead.calculate_overhead_percentage(Overhead.Overhead_cost)
+    cashonhand_results = CashOnHand.analyze_cash_data(CashOnHand.cash_data)
+    profitnloss_results = profits_loss.calculate_net_profit_deficit_or_highest_increment(profits_loss.data)
+    return f'{overhead_result}\n{cashonhand_results}\n{profitnloss_results}'
 
-main()
+# main()
+main = str(main())
+
+with open('summary_report.txt', 'w') as file:
+    file.write(main)
 
