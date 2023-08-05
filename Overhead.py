fa6135e667fb68f045c2715b424a52399f656bdd
Syadnitsu <139613071+Syadnitsu@users.noreply.github.com>
2023-08-05 14:38:15 +0800

    Update Overhead.py

--- Overhead.py
+++ Overhead.py
@@ -15,43 +15,41 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         # get the Day, Items, Note, Amount for each record and append the Overhead_cost list
         Overhead_cost.append([row[0],row[1],row[2],row[3]])   
 
-def Overhead():
-    def calculate_overhead_percentage(Overhead_cost):
-        expenses = [] # create an empty list
-        # initialise variables
-        total_amount = 0
-        highest_percentage = 0
-        highest_category = ""
+# def overhead():
+def calculate_overhead_percentage(Overhead_cost):
+    expenses = [] # create an empty list
+    # initialise variables
+    total_amount = 0
+    highest_percentage = 0
+    highest_category = ""
 
-        # append expenses to expenses list
+    # append expenses to expenses list
+    for item in Overhead_cost:
+        if item[1] not in expenses:
+            expenses.append(item[1])
+
+    # calculate the total expense  
+    for expense in expenses:
+        category_amount = 0
         for item in Overhead_cost:
-            if item[1] not in expenses:
-                expenses.append(item[1])
+            if item[1] == expense:
+                category_amount += int(item[3])
+        total_amount += category_amount
 
-        # calculate the total expense  
-        for expense in expenses:
-            category_amount = 0
-            for item in Overhead_cost:
-                if item[1] == expense:
-                    category_amount += int(item[3])
-            total_amount += category_amount
+    # calculate expense for each category
+    for expense in expenses:
+        category_amount = 0
+        for item in Overhead_cost:
+            if item[1] == expense:
+                category_amount += int(item[3])
+        # find the percentage of expense for each category
+        percentage = (category_amount / total_amount) * 100
 
-        # calculate expense for each category
-        for expense in expenses:
-            category_amount = 0
-            for item in Overhead_cost:
-                if item[1] == expense:
-                    category_amount += int(item[3])
-            # find the percentage of expense for each category
-            percentage = (category_amount / total_amount) * 100
+        # compare and find the highest percentage and category
+        if percentage > highest_percentage:
+            highest_percentage = round(percentage,2)
+            highest_category = expense
 
-            # compare and find the highest percentage and category
-            if percentage > highest_percentage:
-                highest_percentage = round(percentage,2)
-                highest_category = expense
-
-        # return and print the highest overhead + category
-        return f'[HIGHEST OVERHEAD] {highest_category} : {highest_percentage}%' 
-    print(calculate_overhead_percentage(Overhead_cost))
-
-Overhead()
+    # return and print the highest overhead + category
+    return f'[HIGHEST OVERHEAD] {highest_category} : {highest_percentage}%' 
+print(calculate_overhead_percentage(Overhead_cost))
